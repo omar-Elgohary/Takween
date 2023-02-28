9862a70706c4b20e586dc7adf3b4160422fa1559
Omar Elgohary <omaregohary82@gmail.com>
2023-02-28 10:51:45 +0200

    sssss

--- resources/views/layouts/component/modal/updateuserprofile.blade.php
+++ resources/views/layouts/component/modal/updateuserprofile.blade.php
@@ -60,7 +60,7 @@
     </div>
 </div>
 
-
+////sss
 <div class="modal fade" id="changepassord" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
