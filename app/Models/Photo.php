fe8328d7a35846eb675fd1509e1e89d996f81045
Omar Elgohary <omaregohary82@gmail.com>
2023-03-20 13:59:34 +0200

    new

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -13,13 +13,12 @@ class Photo extends Model
 
     public function likes(){
         return $this->morphMany(Like::class,"likesable");
- 
-        
     }
-        public function carts(){
-            return $this->morphMany(Cart::class,"cartsable");
-        }   
-        public function sells(){
-            return $this->morphMany(Selled::class,"selledsable");
-        }   
+
+    public function carts(){
+        return $this->morphMany(Cart::class,"cartsable");
+    }
+    public function sells(){
+        return $this->morphMany(Selled::class,"selledsable");
+    }
 }
