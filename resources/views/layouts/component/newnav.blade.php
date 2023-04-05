6bdb3c693e18584d7a5dbe2ff39818a74690bd64
ahmedessawy10 <elessawy238@gmail.com>
2023-04-06 01:49:00 +0200

    ss

--- resources/views/layouts/component/newnav.blade.php
+++ resources/views/layouts/component/newnav.blade.php
@@ -116,7 +116,7 @@
 </div>
 
     <div class="logo">
-        <img src="{{asset("assets/images/newlogo.png")}}" alt="">
+        <img src="{{asset("assets/images/newlogo2.png")}}" alt="">
     </div>
 </div>
 
